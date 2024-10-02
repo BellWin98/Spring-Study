ba2b3cefa06b476b6396dbcd4b0ae225f043d948
BellWin98 <jongseunghan@naver.com>
2024-10-02 16:12:26 +0900

    DIP 원칙을 준수하여 회원, 주문 서비스 테스트 코드 작성
    * BeforeEach 어노테이션을 통해 각 테스트 실행 전, AppConfig 클래스를 호출하여 인터페이스 구현체의 인스턴스 참조를 생성자를 통해 주입

---
+++ src/test/java/com/basic/core/order/OrderServiceTest.java
@@ -0,0 +1,32 @@
+package com.basic.core.order;
+
+import com.basic.core.AppConfig;
+import com.basic.core.member.Grade;
+import com.basic.core.member.Member;
+import com.basic.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+
+        memberService.join(new Member(memberId, "memberA", Grade.VIP));
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
