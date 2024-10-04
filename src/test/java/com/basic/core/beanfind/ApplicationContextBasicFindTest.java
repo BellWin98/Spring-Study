1f9df7a71510783bacbb1261e6debd27f401a4d6
BellWin98 <jongseunghan@naver.com>
2024-10-04 11:39:55 +0900

    스프링 컨테이너에 등록된 빈 조회
    * 스프링 내부에서 사용하는 빈과, 개발자가 직접 등록한 애플리케이션 빈 조회
    * 여러가지 방식으로 빈 조회 (이름, 타입, 구체타입, 실패 테스트)

---
+++ src/test/java/com/basic/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,47 @@
+package com.basic.core.beanfind;
+
+import com.basic.core.AppConfig;
+import com.basic.core.member.MemberService;
+import com.basic.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 빈 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 빈 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 불가")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
