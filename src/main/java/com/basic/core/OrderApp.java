d2f1d268b8d7aa2a070a668c1afc0ae75c4f9a69
BellWin98 <jongseunghan@naver.com>
2024-10-04 11:11:56 +0900

    스프링 컨테이너와 빈에 대한 설명 주석으로 추가
    * 스프링 컨테이너가 @Bean이 붙은 메서드를 탐색해서 스프링 컨테이너에 등록 -> 등록된 빈들 간의 의존 관계를 설정하고 주입
    * 빈 사용법

--- src/main/java/com/basic/core/OrderApp.java
+++ src/main/java/com/basic/core/OrderApp.java
@@ -18,6 +18,11 @@ public class OrderApp {
         /**
          * ApplicationContext를 스프링 컨테이너라고 함
          * 스프링 빈은 @Bean 이 붙은 메서드의 이름을 스프링 빈의 이름으로 사용
+         * AnnotationConfigApplicationContext는 ApplicationContext의 구현체
+         * 스프링 컨테이너는 파라미터로 넘어온 설정 클래스 정보(AppConfig.class)를 사용해서 스프링 빈을 등록함
+         * 빈의 이름은 메서드 이름으로 사용하고, 직접 부여할 수도 있음 (@Bean(name="memberService2"))
+         * 단, 빈 이름은 항상 다른 이름을 부여하기, 중복되면 다른 빈이 무시되거나, 기존 빈을 덮어버려 오류 발생
+         * 스프링 컨테이너는 빈을 등록한 후에, 설정 정보(AppConfig.class)를 참고해서 빈 간의 의존관계를 주입(DI)한다.
          */
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = ac.getBean("memberService", MemberService.class);
