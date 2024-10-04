4b905d684e8aed19f1e8e054b3edffe5cd63ed04
BellWin98 <jongseunghan@naver.com>
2024-10-04 14:16:53 +0900

    XML 기반으로 스프링 설정 정보 등록

---
+++ src/test/java/com/basic/core/xml/XmlAppContext.java
@@ -0,0 +1,19 @@
+package com.basic.core.xml;
+
+import com.basic.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    @DisplayName("빈을 appConfig.xml 에서 가져온다")
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
