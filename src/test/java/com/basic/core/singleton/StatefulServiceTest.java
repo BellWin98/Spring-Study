3d46dd036dcbd9ad99d5bc690316f74e4b20df6f
BellWin98 <jongseunghan@naver.com>
2024-10-04 16:34:13 +0900

    싱글톤 패턴 사용 시 주의점 개선
    * 공유할 수 있는 필드 제거 후, 지역변수, 파라미터 사용해서 개선
    * 스프링 빈은 항상 무상태로 설계하기!

--- src/test/java/com/basic/core/singleton/StatefulServiceTest.java
+++ src/test/java/com/basic/core/singleton/StatefulServiceTest.java
@@ -15,15 +15,12 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
 
         // ThreadA: A사용자 10,000원 주문
-        statefulService1.order("userA", 10000);
+        int priceA = statefulService1.order("userA", 10000);
+
         // ThreadB: B사용자 20,000원 주문
-        statefulService2.order("userB", 20000);
+        int priceB = statefulService2.order("userB", 20000);
 
-        int price = statefulService1.getPrice(); // A사용자 주문 금액 10,000원을 기대했으나, 20,000원 출력
-
-        System.out.println("price = " + price);
-
-        assertThat(price).isEqualTo(20000);
+        assertThat(priceA).isEqualTo(10000);
     }
 
     static class TestConfig {
