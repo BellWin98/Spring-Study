750e7cea7b73f61ff3943a3e91f50c895ce1c811
BellWin98 <jongseunghan@naver.com>
2024-10-11 11:11:23 +0900

    컴포넌트 스캔에서 같은 빈 이름을 등록
    * 자동으로 등록된 빈 들끼리 충돌나면, 예외 발생 (ConflictingBeanDefinitionException)
    * 수동 빈 등록과 자동 빈 등록에서 빈 이름이 충돌나면, 수동 빈 등록이 우선권을 가져서 덮어쓰기 됨
    * 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꿈. application.properties 에 코드 true 로 설정하면 오류 안남

--- src/main/java/com/basic/core/AutoAppConfig.java
+++ src/main/java/com/basic/core/AutoAppConfig.java
@@ -1,18 +1,26 @@
 package com.basic.core;
 
+import com.basic.core.member.MemberRepository;
+import com.basic.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-@Configuration
 
 // @Component 어노테이션이 붙은 클래스를 찾아서 자동으로 스프링 빈으로 등록해줌
 @ComponentScan(
         // 패키지 위치를 지정하지 않으면, @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 됨
-        basePackages = "com.basic.core.member",
+//        basePackages = "com.basic.core.member",
 //        basePackageClasses = AutoAppConfig.class, // AutoAppConfig.class 가 있는 패키지의 하위 패키지를 모두 탐색
         // @Configuration 클래스는 컴포넌트 스캔 대상에서 제외 (기존 예제 코드 유지하기 위해 충돌 방지)
         excludeFilters = @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
 )
+@Configuration
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
